93f580869cf43127102539f77602342c7ebad819
KY00KIM <dev.kyoomin@gmail.com>
2024-01-11 02:18:06 +0800

    Add p10k config

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ ln -s -f ~/.dotfiles/.zlogin ~/.zlogin
 ln -s -f ~/.dotfiles/.vimrc ~/.vimrc
 ln -s -f ~/.dotfiles/.tmux/.tmux.conf ~/.tmux.conf
 ln -s -f ~/.dotfiles/.tmux.conf.local ~/.tmux.conf.local
+ln -s -f ~/.dotfiles/themes/.p10k.zsh ~/.p10k.zsh
 
 cp -n ~/.dotfiles/.zshenv ~/.zshenv
 cp -n ~/.dotfiles/.gitconfig ~/.gitconfig
